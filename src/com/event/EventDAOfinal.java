7c6859247537a4caf564ada22f2689d40c85b1b5
AAYUSHI PUROHIT <176600701+aayushipurohit15@users.noreply.github.com>
2026-04-08 22:17:32 +0530

    final changes made and errors solved

---
+++ src/com/event/EventDAOfinal.java
@@ -0,0 +1,99 @@
+package com.event.dao;
+import java.sql.*;
+
+public class EventDAO {
+
+    public void addEvent(String name, String date, String location, double price) {
+        try {
+            Connection con = DBConnection.getConnection();
+            String query = "INSERT INTO events(name,date,location,price) VALUES(?,?,?,?)";
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, date);
+            ps.setString(3, location);
+            ps.setDouble(4, price);
+
+            ps.executeUpdate();
+            System.out.println("Event Added");
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void viewEvents() {
+        try {
+            Connection con = DBConnection.getConnection();
+            ResultSet rs = con.createStatement().executeQuery("SELECT * FROM events");
+
+            while(rs.next()) {
+                System.out.println(
+                    rs.getInt("id") + " " +
+                    rs.getString("name") + " " +
+                    rs.getString("date") + " " +
+                    rs.getString("location") + " " +
+                    rs.getDouble("price")
+                );
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void deleteEvent(int id) {
+        try {
+            Connection con = DBConnection.getConnection();
+            String query = "DELETE FROM events WHERE id=?";
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+            System.out.println("Event Deleted");
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void updateEvent(int id, String name) {
+        try {
+            Connection con = DBConnection.getConnection();
+            String query = "UPDATE events SET name=? WHERE id=?";
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setInt(2, id);
+
+            ps.executeUpdate();
+            System.out.println("Event Updated");
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void searchEvent(String keyword) {
+        try {
+            Connection con = DBConnection.getConnection();
+            String query = "SELECT * FROM events WHERE name LIKE ?";
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, "%" + keyword + "%");
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(
+                    rs.getInt("id") + " " +
+                    rs.getString("name") + " " +
+                    rs.getString("date") + " " +
+                    rs.getString("location") + " " +
+                    rs.getDouble("price")
+                );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
